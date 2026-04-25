92121a7f594ff241f12ed9909eabb96160bc224a
linuxosuser <154092858+linuxosuser@users.noreply.github.com>
2026-04-25 13:44:50 +0200

    Create videocreate.sh

---
+++ videocreate.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+ffmpeg -loop 1 -i "$1" -i "$2" -c:v libx264 -b:v 4000k -minrate 4000k -maxrate 4000k -bufsize 8000k -vf "fps=60,scale=1920:1080" -c:a libmp3lame -b:a 320k -pix_fmt yuv420p -shortest "$3"
+
+echo 'A VIDEÓ ELKÉSZÜLT.'
